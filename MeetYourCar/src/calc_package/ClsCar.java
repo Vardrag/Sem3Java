cb987598d487de8986f856cb6a273c1fde156018
Vardrag <vardrag@gmx.de>
2014-01-27 13:39:57 +0100

    nur ein Kommentar

--- MeetYourCar/src/calc_package/ClsCar.java
+++ MeetYourCar/src/calc_package/ClsCar.java
@@ -4,12 +4,13 @@ public class ClsCar {
 private int intKilometerstand; //Kilometerstand des Autos
 private String strAutofirma; //Herstellerfirma des Autos
 private String strFarbe; //Farbe des Autos, als Name (evtl. auch als Farbcode angebbar)
+private String strSQLarg;
 
 public void setFarbe(String eingFarbe){
 	strFarbe = eingFarbe;
 }
 
-public String getFarbe() {
+public String getFarbe()/*Ausgabe Lackfarbe des Autos*/ {
 	return strFarbe;
 }
 
@@ -29,8 +30,9 @@ public void setKilometerstand(int intKilometerstand) {
 	this.intKilometerstand = intKilometerstand;
 }
 
-public void addCar(){
-	
+public String addCar()/*Objekt zu der Tabelle "Angebot" hinzufügen; gibt SQL-String weiter*/{
+	strSQLarg = "Insert into Angebot VALUES Angebot.Farbe ="  + strFarbe + ", Angebot.Herrsteller = ";
+	return strSQLarg;
 }
 
 
