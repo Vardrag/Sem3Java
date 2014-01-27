5a12373b2e8dd318c7f3a8d4a5aee01736416c72
Vardrag <vardrag@gmx.de>
2014-01-27 08:13:27 +0100

    Autoklasse

--- MeetYourCar/src/calc_package/ClsCar.java
+++ MeetYourCar/src/calc_package/ClsCar.java
@@ -1,5 +1,39 @@
 package calc_package;
 
 public class ClsCar {
+private int intKilometerstand; //Kilometerstand des Autos
+private String strAutofirma; //Herstellerfirma des Autos
+private String strFarbe; //Farbe des Autos, als Name (evtl. auch als Farbcode angebbar)
+
+public void setFarbe(String eingFarbe){
+	strFarbe = eingFarbe;
+}
+
+public String getFarbe() {
+	return strFarbe;
+}
+
+public String getAutofirma() {
+	return strAutofirma;
+}
+
+public void setAutofirma(String strAutofirma) {
+	this.strAutofirma = strAutofirma;
+}
+
+public int getKilometerstand() {
+	return intKilometerstand;
+}
+
+public void setKilometerstand(int intKilometerstand) {
+	this.intKilometerstand = intKilometerstand;
+}
+
+public void addCar(){
+	
+}
+
+
+
 
 }
