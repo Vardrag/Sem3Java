9de0a88e3781a7b376a483c324e28a157b84f854
Vardrag <vardrag@gmx.de>
2014-01-29 10:16:01 +0100

    Erweiterung Car.cls um einige Variablen

--- MeetYourCar/src/ressourcePackage/Car.java
+++ MeetYourCar/src/ressourcePackage/Car.java
@@ -1,10 +1,18 @@
 package ressourcePackage;
 
+import java.util.Currency;
+
 public class Car {
 private int intKilometerstand; //Kilometerstand des Autos
-private String strAutofirma; //Herstellerfirma des Autos
+private String strAutofirma; //Marke des Autos
 private String strFarbe; //Farbe des Autos, als Name (evtl. auch als Farbcode angebbar)
 private String strSQLarg;
+private int datErstzul; //Erstzulassung
+private String strKrSt; //Kraftstoff
+private int intKmStand; //Kilometerstand
+private String strLeistung; //Motorleistung
+private Currency crcPreis; //Angebotspreis
+private String strSonst; //Sonstiges; vorübergehend
 
 public void setFarbe(String eingFarbe){
 	strFarbe = eingFarbe;
@@ -28,6 +36,54 @@ public int getKilometerstand() {
 
 public void setKilometerstand(int intKilometerstand) {
 	this.intKilometerstand = intKilometerstand;
+}
+
+public int getDatErstzul() {
+	return datErstzul;
+}
+
+public void setDatErstzul(int datErstzul) {
+	this.datErstzul = datErstzul;
+}
+
+public String getStrKrSt() {
+	return strKrSt;
+}
+
+public void setStrKrSt(String strKrSt) {
+	this.strKrSt = strKrSt;
+}
+
+public int getIntKmStand() {
+	return intKmStand;
+}
+
+public void setIntKmStand(int intKmStand) {
+	this.intKmStand = intKmStand;
+}
+
+public String getStrLeistung() {
+	return strLeistung;
+}
+
+public void setStrLeistung(String strLeistung) {
+	this.strLeistung = strLeistung;
+}
+
+public Currency getCrcPreis() {
+	return crcPreis;
+}
+
+public void setCrcPreis(Currency crcPreis) {
+	this.crcPreis = crcPreis;
+}
+
+public String getStrSonst() {
+	return strSonst;
+}
+
+public void setStrSonst(String strSonst) {
+	this.strSonst = strSonst;
 }
 
 public void addCar()/*Objekt zu der Tabelle "Angebot" hinzufügen; gibt SQL-String weiter*/{
