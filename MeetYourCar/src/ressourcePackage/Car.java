652a5dd2e932b66d86d7af4d2dd99867c7c94669
Vardrag <vardrag@gmx.de>
2014-01-29 12:49:40 +0100

    Dokumentation auf Google Drive verschoben --macht sich besser

--- MeetYourCar/src/ressourcePackage/Car.java
+++ MeetYourCar/src/ressourcePackage/Car.java
@@ -3,89 +3,23 @@ package ressourcePackage;
 import java.util.Currency;
 
 public class Car {
-private int intKilometerstand; //Kilometerstand des Autos
-private String strAutofirma; //Marke des Autos
+//Variablen
+public int intKilometerstand; //Kilometerstand des Autos
+public String strAutofirma; //Marke des Autos
 private String strFarbe; //Farbe des Autos, als Name (evtl. auch als Farbcode angebbar)
-private String strSQLarg;
-private int datErstzul; //Erstzulassung
-private String strKrSt; //Kraftstoff
-private int intKmStand; //Kilometerstand
-private String strLeistung; //Motorleistung
-private Currency crcPreis; //Angebotspreis
-private String strSonst; //Sonstiges; vorübergehend
+public String strSQLarg;
+public int datErstzul; //Erstzulassung
+public String strKrSt; //Kraftstoff
+public int intKmStand; //Kilometerstand
+public String strLeistung; //Motorleistung
+public Currency crcPreis; //Angebotspreis
+public String strSonst; //Sonstiges; vorübergehend
 
-public void setFarbe(String eingFarbe){
-	strFarbe = eingFarbe;
-}
 
-public String getFarbe()/*Ausgabe Lackfarbe des Autos*/ {
-	return strFarbe;
-}
+//private Methoden
 
-public String getAutofirma() /*Ausgabe Herrsteller des Autos*/{
-	return strAutofirma;
-}
 
-public void setAutofirma(String strAutofirma) {
-	this.strAutofirma = strAutofirma;
-}
-
-public int getKilometerstand() {
-	return intKilometerstand;
-}
-
-public void setKilometerstand(int intKilometerstand) {
-	this.intKilometerstand = intKilometerstand;
-}
-
-public int getDatErstzul() {
-	return datErstzul;
-}
-
-public void setDatErstzul(int datErstzul) {
-	this.datErstzul = datErstzul;
-}
-
-public String getStrKrSt() {
-	return strKrSt;
-}
-
-public void setStrKrSt(String strKrSt) {
-	this.strKrSt = strKrSt;
-}
-
-public int getIntKmStand() {
-	return intKmStand;
-}
-
-public void setIntKmStand(int intKmStand) {
-	this.intKmStand = intKmStand;
-}
-
-public String getStrLeistung() {
-	return strLeistung;
-}
-
-public void setStrLeistung(String strLeistung) {
-	this.strLeistung = strLeistung;
-}
-
-public Currency getCrcPreis() {
-	return crcPreis;
-}
-
-public void setCrcPreis(Currency crcPreis) {
-	this.crcPreis = crcPreis;
-}
-
-public String getStrSonst() {
-	return strSonst;
-}
-
-public void setStrSonst(String strSonst) {
-	this.strSonst = strSonst;
-}
-
+//öffentliche Methoden
 public void addCar()/*Objekt zu der Tabelle "Angebot" hinzufügen; gibt SQL-String weiter*/{
 	strSQLarg = "Insert into Angebot VALUES Angebot.Farbe ="  + strFarbe + ", Angebot.Herrsteller = ";
 }
--- MeetYourCar/src/ressourcePackage/FunctCar.java
+++ MeetYourCar/src/ressourcePackage/FunctCar.java
@@ -3,15 +3,43 @@ package ressourcePackage;
 
 public class FunctCar /* Stellt Funktionen für die Arbeit mit Objekten der Klasse "ClsCar" bereit*/ {
 
-	public Car[] searchCars(Car referenzCar)/*Sucht Eine Liste von Autos, muss für Liste gemacht werden (Collection??)*/{
-		 //sollte Collection/Array werden
-		//Hier SqlString aus referenzCar generieren
-		//Hier SqlString an DBKlasse weitergeben
-		Car resultCar[] = null;
-		for (int i = 0; i < 10; i++) { //Füllen durch Ergebnis der Abfrage
-			resultCar[i] = new Car(/*Hier werden die Daten aus der DBAbfrage eingefügt*/);
+	public Car[] searchCars(Car referenzCar)/*Sucht eine Gruppe von Autos*/{
+
+		StringBuilder strB = new StringBuilder();
+		
+		//Beginn SQL-String bauen
+		strB.append("Select * from ");
+		strB.append("Tabelle"); //Tabellenname Fehlt
+		strB.append(" Where ");
+		if (referenzCar.strAutofirma != null) //SQL-Bedingung "Marke"
+		{
+			//Spalte plus Bedingung
+			//Variable	
 		}
-		return resultCar;	//Conection zurückgeben, um angezeigt zu werden	
+		if (referenzCar.crcPreis != null) //SQL-Bedingung "Preis"
+		{
+			//Spalte plus Bedingung
+			//Variable	
+		}		
+		if (referenzCar.datErstzul != 0) //SQL-Bedingung "Erstzulassung"
+		{
+			//Spalte plus Bedingung
+			//Variable	
+		}		
+		
+		// Ende SQL-String bauen
+		referenzCar.strSQLarg = strB.toString();
+	
+		//DB-Abfrage einfügen Eingabe referenzCar.strSQLarg Ausgabe ...
+		
+		Car resultCar[] = dbToCar(/*DB-Rückgabe*/);
+		
+		return resultCar; //gibt Car-Array zur Ausgabe zurück
 	}
 	
+	private Car[] dbToCar(){ //Konvertiert die DB-Rückgabe in ein Car-Arryay zur Weiterbearbeitung
+		
+		return null;
+		
+	}
 }
