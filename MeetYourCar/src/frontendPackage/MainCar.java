f19eb22cb59aeffe3003849e4e5610c74cc09a39
borisch <borisch@ntpc77.dd.systemagmbh.de>
2014-02-07 07:56:57 +0100

    @Mafi / add Variablen in User.java

--- MeetYourCar/src/frontendPackage/MainCar.java
+++ MeetYourCar/src/frontendPackage/MainCar.java
@@ -1,46 +1,50 @@
 package frontendPackage;
 
 import java.io.*;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 import java.net.*;
 
 import ressourcePackage.Car;
-
+import ressourcePackage.User;
 
 
 public class MainCar extends HttpServlet
 {
    static final long serialVersionUID = 1L;
   
-   Car carObj = new Car();
-   @Override
+   Car carObj = new Car(); //Car object anlegen
+   User userObj = new User(); // User object anlegen
+   
+   
    //Abfrage per Post
+   
+  public void InsertUser(){
+	   
+   }
+   
+  @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) 
 	          throws ServletException, IOException {
 	   
 	   Object strAutofirma = request.getParameter("Marke");
 	   Object strLeistung = request.getParameter("Leistung");
 	   
-	   //Parameter 
+	   //Parameter für Marke
 	   StringBuffer sb = new StringBuffer((String) strAutofirma);
 	   String ausgabe_Marke = sb.toString();   
-	   //session handle
+	   //session handle für Marke
 	   HttpSession Marke = request.getSession();
 	   Marke.setAttribute("Marke", ausgabe_Marke);
 	   	  	   
-	   //Parameter
+	   //Parameter für Leistung
 	   StringBuffer tb = new StringBuffer((String) strLeistung);
 	   String ausgabe_Leistung = tb.toString();
-	   //session handle
+	   //session handle für Leistung
 	   HttpSession Leistung = request.getSession();
 	   Leistung.setAttribute("Leistung", ausgabe_Leistung);
 	   
-	   
-	 
-	   
+	  
 	   
 	   
 	   
--- MeetYourCar/src/ressourcePackage/User.java
+++ MeetYourCar/src/ressourcePackage/User.java
@@ -2,11 +2,17 @@ package ressourcePackage;
 
 public class User {
 //Variablen
+private int intID; //ID des Users (autoFeld der DB)
+private String strUsername; // Benutzername des Users
 private String strVorname; //Vorname des Users
 private String strNachname;// Nachname des Users
-private int intID; //ID des Users (autoFeld der DB)
 private String strEmail; // Emailadresse des Users
+private String strPasswort; // Passwort des Users
 
+//ab hier Zusatz (keine Pflichtfelder - edit profiloptions)
+private String strAlter; // Alter des Users
+private String strTelefon; // TelefonNr des Users
+private String strStandortCar; //allgemeiner Standort (Wohnort des Users)
 
 //private Methoden
 private int loadID()/*lädt ID aus DB nach*/{
@@ -42,6 +48,7 @@ public void setStrEmail(String strEmail) {
 public void addUser()/*fügt neuen User der DB hinzu*/{
 	//User hinzufügen
 	intID = loadID();
+	
 }
 
 public void updateUser/*Sucht DBEintrag nach ID und updatet alle anderen Felder*/(){
