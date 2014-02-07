88bae3ffc8ecc50a53a5b099c9e7d37f3c9cf122
borisch <borisch@ntpc77.dd.systemagmbh.de>
2014-02-07 09:17:34 +0100

    !!!! HSQLDB UPLOAD !!!! meetyourcar -> webcontent -> hsqldb (path datenbank) new package db_connection + new class DB_connection (Klasse für Datenbankzugriff)

---
+++ MeetYourCar/src/db_connection/DB_connection.java
@@ -0,0 +1,72 @@
+package db_connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+ 
+public class DB_connection
+{
+    public DB_connection()
+    {
+          
+    try
+    {
+        // Treiberklasse laden
+      Class.forName( "org.hsqldb.jdbc.JDBCDriver" );
+    }
+    catch ( ClassNotFoundException e )
+    {
+      System.err.println( "Treiberklasse nicht gefunden!" );
+      return;
+    }
+  
+    Connection con = null;
+  
+    try
+    {
+      con = DriverManager.getConnection( 
+              "jdbc:hsqldb:file:MeetYourCar/WebContent/HSQLDB/hsqldb-2.3.1/hsqldb; shutdown=true", "root", "root" );
+      Statement stmt = con.createStatement();
+  
+      // Alle Kunden ausgeben
+      String sql = "SELECT * FROM Customer";
+      ResultSet rs = stmt.executeQuery(sql);
+  
+      while ( rs.next() )
+      {
+        String id = rs.getString(1);
+        String firstName = rs.getString(2);
+        String lastName = rs.getString(3);
+        System.out.println(id + ", " + firstName + " " + lastName);
+      }
+       
+      // Resultset schlieﬂen
+      rs.close();
+  
+      // Statement schlieﬂen
+      stmt.close();
+    }
+    catch ( SQLException e )
+    {
+      e.printStackTrace();
+    }
+    finally
+    {
+      if ( con != null )
+      {
+        try {
+            con.close();
+            } catch ( SQLException e ) {
+                e.printStackTrace();
+            }
+      }
+    }
+    }
+     
+    public static void main(String[] args)
+    {
+        new DB_connection();
+    }
+}
